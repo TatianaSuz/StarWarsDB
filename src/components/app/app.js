39834a584b685d0a37ec622de17a2bc09e6d7e10
Tanya <you@example.com>
2022-03-24 21:15:45 +0300

    completed the course 'work with the server'

---
+++ src/components/app/app.js
@@ -0,0 +1,26 @@
+import './app.css'
+import Header from "../header";
+import RandomPlanet from '../random-planet';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+
+import './app.css';
+
+const App = () => {
+    return (
+        <div className='tablet'>
+            <Header />
+            <RandomPlanet />
+            <div className="row mb2">
+                <div className="col-md-6">
+                    <ItemList />
+                </div>
+                <div className="col-md-6">
+                    <PersonDetails />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-4052agrp3sb">
+            <div className="ldio-881arq9zvo">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner
