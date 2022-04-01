7464cf481acc173d681761067d69080a9de7a3cd
Tanya <you@example.com>
2022-04-01 21:13:59 +0300

    Ready for deploy

---
+++ docs/precache-manifest.5f12e087bdc0c35b2d418e9b9d58d252.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccf74274e0773f33d7ac",
+    "url": "/static/css/main.0d01109f.chunk.css"
+  },
+  {
+    "revision": "ccf74274e0773f33d7ac",
+    "url": "/static/js/main.ccf74274.chunk.js"
+  },
+  {
+    "revision": "db5c04c76219c80f223a",
+    "url": "/static/js/1.db5c04c7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8faa3debea45fb352093a9c6646d1089",
+    "url": "/index.html"
+  }
+];
